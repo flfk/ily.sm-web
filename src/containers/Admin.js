6cde1b2153136ac1a8b1389e4ce899056a79999f
flfk <flfk@users.noreply.github.com>
2018-11-29 18:01:46 -0300

    Added admin page

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -6,6 +6,7 @@ import Content from '../components/Content';
 import { COMMISSION } from '../utils/Constants';
 import Countdown from '../components/Countdown';
 import Fonts from '../utils/Fonts';
+import { getDateAddDays } from '../utils/Helpers';
 import Spinner from '../components/Spinner';
 
 class Admin extends React.Component {
@@ -24,10 +25,16 @@ class Admin extends React.Component {
   setData = async () => {
     const influencers = await actions.fetchDocsInfluencers();
     const ordersSeperated = await Promise.all(
-      influencers.map(async influencer => await actions.fetchDocsOrders(influencer.id))
+      influencers.map(async influencer => actions.fetchDocsOrders(influencer.id))
     );
     const orders = _.flatten(ordersSeperated);
     this.setState({ influencers, orders, isLoading: false });
+  };
+
+  sortByDisplayName = (a, b) => {
+    if (a.displayName < b.displayName) return -1;
+    if (a.displayName > b.displayName) return 1;
+    return 0;
   };
 
   render() {
@@ -37,28 +44,34 @@ class Admin extends React.Component {
     const revenueIlysm = revenueTotal * COMMISSION;
     const revenueInfluencers = revenueTotal * (1 - COMMISSION);
 
-    const influencerStats = influencers
-      .sort((a, b) => a.displayName - b.displayName)
-      .map(influencer => {
-        const influencerOrders = orders.filter(order => order.influencerID === influencer.id);
-        const influencerRevenue = this.getRevenue(influencerOrders);
-        return (
-          <div>
-            <Fonts.H3>{influencer.displayName}</Fonts.H3>
-            <Fonts.P>
-              <strong>$ {influencerRevenue.toFixed(2)}</strong> total revenue
-            </Fonts.P>
-            <Fonts.P>
-              <strong>$ {(influencerRevenue * COMMISSION).toFixed(2)}</strong> ily.sm revenue
-            </Fonts.P>
-            <Fonts.P>
-              <strong>$ {(influencerRevenue * (1 - COMMISSION)).toFixed(2)}</strong> influencer
-              revenue
-            </Fonts.P>
-            <Content.Spacing8px />
-          </div>
-        );
-      });
+    const influencerStats = influencers.sort(this.sortByDisplayName).map(influencer => {
+      const influencerOrders = orders.filter(order => order.influencerID === influencer.id);
+      const influencerRevenue = this.getRevenue(influencerOrders);
+      const dateUpdateNext = getDateAddDays(influencer.dateUpdateLast, 7);
+      return (
+        <div key={influencer.id}>
+          <Fonts.H3 centered>{influencer.displayName}</Fonts.H3>
+          <Content.Row justifyStart>
+            <Countdown date={dateUpdateNext} small />
+            <Fonts.P>until next update</Fonts.P>
+          </Content.Row>
+          <Fonts.P>
+            <strong>$ {influencerRevenue.toFixed(2)}</strong> total revenue
+          </Fonts.P>
+          <Fonts.P>
+            <strong>$ {(influencerRevenue * COMMISSION).toFixed(2)}</strong> ily.sm revenue
+          </Fonts.P>
+          <Fonts.P>
+            <strong>$ {(influencerRevenue * (1 - COMMISSION)).toFixed(2)}</strong> influencer
+            revenue
+          </Fonts.P>
+          <Fonts.P>
+            <strong>{influencerOrders.length}</strong> gifts
+          </Fonts.P>
+          <Content.Spacing8px />
+        </div>
+      );
+    });
 
     if (isLoading) return <Spinner />;
 
